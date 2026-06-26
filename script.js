2f38c52d040283f7c3048fbccd8c077afa881181
abdelrahmanacc5-byte <abdelrahmanacc5@gmail.com>
2026-06-26 22:22:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,42 @@ document.getElementById("loader").style.display="none";
 },1800);
 
 }
+/*================ COUNTER =================*/
+
+const counters=document.querySelectorAll(".counter");
+
+const startCounter=()=>{
+
+counters.forEach(counter=>{
+
+const target=+counter.dataset.target;
+
+let count=0;
+
+const update=()=>{
+
+const increment=Math.ceil(target/80);
+
+count+=increment;
+
+if(count<target){
+
+counter.innerText=count;
+
+requestAnimationFrame(update);
+
+}else{
+
+counter.innerText=target;
+
+}
+
+};
+
+update();
+
+});
+
+};
+
+window.addEventListener("load",startCounter);
