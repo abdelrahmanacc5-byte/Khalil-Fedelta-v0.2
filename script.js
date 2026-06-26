a7dd72d76fdcff97c2a0ad4f2f0abaa0f02bd981
abdelrahmanacc5-byte <abdelrahmanacc5@gmail.com>
2026-06-26 22:18:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+/* =========================
+   LOADING
+========================= */
 
+window.onload=function(){
+
+setTimeout(function(){
+
+document.getElementById("loader").style.opacity="0";
+
+setTimeout(function(){
+
+document.getElementById("loader").style.display="none";
+
+},700);
+
+},1800);
+
+}
